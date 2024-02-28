757341f04529ebd4f26643b02a73d9c745ca6579
LucaYan0506 <lucayan0506@gmail.com>
2024-02-28 23:10:12 +0000

    database almost complete, successfully connect react with django, APIs for getting the form & submitting the form complete. API for the report (in progress)

--- Challenge1/core/models.py
+++ Challenge1/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class FormTemplate(models.Model):
+    code = models.CharField(max_length = 10, primary_key=True)
+    gender_active = models.BooleanField(default=True)
+    pronouns_active = models.BooleanField(default=True)
+    age_active = models.BooleanField(default=True)
+    ethnicity_active = models.BooleanField(default=True)
+    disabilitiy_active = models.BooleanField(default=True)
+    employment_status_active = models.BooleanField(default=True)
+
+    @classmethod
+    def get_default_pk(cls):
+            template, created = cls.objects.get_or_create(code='default template')
+            return template.pk
+
+class FormDetail(models.Model):
+    template = models.ForeignKey(FormTemplate, default=FormTemplate.get_default_pk, on_delete=models.DO_NOTHING)
+    gender = models.CharField(max_length=3, blank=True, default='n/a')
+    pronouns = models.CharField(max_length=3, blank=True, default='n/a')
+    age = models.CharField(max_length=3, blank=True, default='n/a')
+    ethnicity = models.CharField(max_length=3, blank=True, default='n/a')
+    disabilitiy = models.CharField(max_length=3, blank=True, default='n/a')
+    employment_status = models.CharField(max_length=3, blank=True, default='n/a')
--- Challenge1/core/urls.py
+++ Challenge1/core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.index_view, name="index"),
-]
+    path('submit/',views.submitForm, name="submit"),
+    path('form/',views.getForm, name="form"),
+    path('report/',views.getReport, name="report"),
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
